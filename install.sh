ed9ef1222ae4897c3e96407eabed97ce737fcb0f
Christoph Van Hees <christoph.vanhees@hotmail.com>
2020-04-22 22:20:58 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-echo "Setting up your Mac..."
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
+echo "Installing dotfiles..."
+
+source symlink.sh
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+  if test ! "$( command -v brew )"; then
+      echo "Installing homebrew"
+      ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+  fi
 
 # Update Homebrew recipes
 brew update
@@ -14,11 +21,25 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
+# after the install, install neovim python libraries
+echo -e "\\n\\nRunning Neovim Python install"
+echo "=============================="
+pip3 install pynvim
+
 # Install PHP extensions with PECL
 pecl install imagick
 
-# Make ZSH the default shell environment
-chsh -s $(which zsh)
+# Change the default shell to zsh
+zsh_path="$( command -v zsh )"
+if ! grep "$zsh_path" /etc/shells; then
+    echo "adding $zsh_path to /etc/shells"
+    echo "$zsh_path" | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    chsh -s "$zsh_path"
+    echo "default shell changed to $zsh_path"
+fi
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
